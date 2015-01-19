1f7eab42b21168a36b3661976a85885fb273b081
Bob Whitelock <bob.whitelock1@gmail.com>
2015-01-19 00:11:20 +0000

    Make picktasks so display message if no tasks available; update paths; and add todo.

--- picktask.py
+++ picktask.py
@@ -4,9 +4,11 @@ import os
 import random
 import sys
 
+# TODO: make delete current task once chosen from todo list
 
-TODO_PATH = 'text_files/todo_temp'
-CURRENT_TASK_PATH = 'text_files/task_temp'
+
+TODO_PATH = 'text_files/todo'
+CURRENT_TASK_PATH = 'text_files/current_task'
 
 COMMENT_MARKER = '#'
 
@@ -16,9 +18,13 @@ def main():
         os.remove(CURRENT_TASK_PATH)
     else:
         current_task = read_current_task()
-        if (current_task == ''):
+        if current_task == '':
             current_task = pick_new_task()
-        print('Chosen task:', current_task)
+
+        if current_task != '':
+            print('Chosen task:', current_task)
+        else:
+            print('No tasks available, add some more.')
 
 
 def read_current_task():
@@ -31,8 +37,11 @@ def read_current_task():
 def pick_new_task():
     lines = [line.strip() for line in open(TODO_PATH).readlines()]
     potential_tasks = [line for line in lines if len(line) > 0 and line[0] != COMMENT_MARKER]
-    current_task = random.choice(potential_tasks)
-    open(CURRENT_TASK_PATH, 'w').write(current_task)
+    if len(potential_tasks) > 0:
+        current_task = random.choice(potential_tasks)
+        open(CURRENT_TASK_PATH, 'w').write(current_task)
+    else:
+        current_task = ''
     return current_task
 
 
